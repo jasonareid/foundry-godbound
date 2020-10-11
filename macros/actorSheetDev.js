50a863b4c5fe069d20683b15db7a1d6d3c51e233
Jason Reid <jason@jasonreid.com>
2020-10-11 12:19:38 -0400

    Some layout and tabs

--- macros/actorSheetDev.js
+++ macros/actorSheetDev.js
@@ -1,15 +1,17 @@
 if(!window.ACTOR_SHEET_DEV_MACRO_FN) {
-    window.ACTOR_SHEET_DEV_MACRO_FN = (ev) => {
-        delete _templateCache["systems/godbound/templates/actor-sheet.html"];
-        game.actors.entities[0].sheet.render(true);
+    window.ACTOR_SHEET_DEV_MACRO_FN = () => {
+        setTimeout(() => {
+            delete _templateCache["systems/godbound/templates/actor-sheet.html"];
+            game.actors.entities[0].sheet.render(true);
 
-        var links = document.getElementsByTagName("link");
-        for (var cl in links)
-        {
-            var link = links[cl];
-            if (link.rel === "stylesheet")
-                link.href += "";
-        }
+            var links = document.getElementsByTagName("link");
+            for (var cl in links)
+            {
+                var link = links[cl];
+                if (link.rel === "stylesheet")
+                    link.href += "";
+            }
+        }, 500);
     };
 }
 if(window.ACTOR_SHEET_DEV_MACRO_ACTIVE) {
@@ -21,4 +23,5 @@ else {
     $(window).on('focus', window.ACTOR_SHEET_DEV_MACRO_FN);
     window.ACTOR_SHEET_DEV_MACRO_ACTIVE = true;
     ui.notifications.info("actor sheet dev activated");
+    window.ACTOR_SHEET_DEV_MACRO_FN();
 }
