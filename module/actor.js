3a415ae1161d32e9885b8852c877db18bbdd1da5
Jason Reid <jason@jasonreid.com>
2020-10-11 07:15:18 -0400

    Template changes

--- module/actor.js
+++ module/actor.js
@@ -1,35 +1,139 @@
-/**
- * Extend the base Actor entity by defining a custom roll data structure which is ideal for the Simple system.
- * @extends {Actor}
- */
 export class GodboundActor extends Actor {
 
-  /** @override */
-  getRollData() {
-    const data = super.getRollData();
-    const shorthand = game.settings.get("godbound", "macroShorthand");
+  static async create(data, options) {
+    let gbActor = await super.create(data, options);
 
-    // Re-map all attributes onto the base roll data
-    if ( !!shorthand ) {
-      for ( let [k, v] of Object.entries(data.attributes) ) {
-        if ( !(k in data) ) data[k] = v.value;
+    if (gbActor.data.type === 'pc') {
+      await gbActor.createOwnedItem({name: 'Fray Die', type: 'autoHitAttack', data: {
+          numDice: 1,
+          diceType: 8
+        }
+      });
+    } else if(gbActor.data.type === 'npc') {
+
+    }
+  }
+
+  prepareData() {
+    super.prepareData();
+    const actorData = this.data;
+
+    // Make separate methods for each Actor type (character, npc, etc.) to keep
+    // things organized.
+    if (actorData.type === 'pc') this._preparePcData(actorData);
+
+    if (actorData.type === 'npc') this._prepareNpcData(actorData);
+  }
+
+  _preparePcData(actorData) {
+    const data = actorData.data;
+
+    // Make a new Object that holds computed data and keeps it separate from anything else
+    data.computed = {};
+
+    data.computed.attributes = {};
+    for(let k of Object.keys(data.attributes)) {
+      const computedAtt = {};
+      const srcAtt = data.attributes[k];
+      const score = srcAtt.score;
+      if(score <= 3) {
+        computedAtt.mod = -3;
+      } else if(score <= 5) {
+        computedAtt.mod = -2;
+      } else if(score <= 8) {
+        computedAtt.mod = -1;
+      } else if(score >= 19) {
+        computedAtt.mod = 4;
+      } else if(score >= 18) {
+        computedAtt.mod = 3;
+      } else if(score >= 16) {
+        computedAtt.mod = 2;
+      } else if(score >= 13) {
+        computedAtt.mod = 1;
+      } else {
+        computedAtt.mod = 0;
       }
-      delete data.attributes;
+      computedAtt.check = 21 - score;
+      data.computed.attributes[k] = computedAtt;
     }
 
-    // Map all items data using their slugified names
-    data.items = this.data.items.reduce((obj, i) => {
-      let key = i.name.slugify({strict: true});
-      let itemData = duplicate(i.data);
-      if ( !!shorthand ) {
-        for ( let [k, v] of Object.entries(itemData.attributes) ) {
-          if ( !(k in itemData) ) itemData[k] = v.value;
-        }
-        delete itemData["attributes"];
-      }
-      obj[key] = itemData;
-      return obj;
-    }, {});
-    return data;
+    data.computed.saves = {};
+    this._prepareSave(data.saves, data.computed.saves, data.computed.attributes, 'hardiness', 'str', 'con');
+    this._prepareSave(data.saves, data.computed.saves, data.computed.attributes, 'evasion', 'dex', 'int');
+    this._prepareSave(data.saves, data.computed.saves, data.computed.attributes, 'spirit', 'wis', 'cha');
+
+    data.computed.armor = {};
+    switch(data.armor.type) {
+      case "light":
+        data.computed.armor.baseAc = 7;
+        break;
+      case "medium":
+        data.computed.armor.baseAc = 5;
+        break;
+      case "heavy":
+      case "divineGift":
+        data.computed.armor.baseAc = 3;
+        break;
+      default:
+        data.computed.armor.baseAc = 9;
+    }
+    let ac = data.computed.armor.baseAc;
+    if(data.armor.shield) {
+      ac -= 1;
+    }
+    ac -= data.armor.bonus;
+    ac -= data.computed.attributes[data.armor.attribute].mod;
+    data.computed.armor.ac = ac;
+    if(data.armor.penalizeHardiness) {
+      data.computed.saves.hardiness.penalty = 4;
+      data.computed.saves.hardiness.save += 4;
+    }
+    if(data.armor.penalizeEvasion) {
+      data.computed.saves.evasion.penalty = 4;
+      data.computed.saves.evasion.save += 4;
+    }
+    if(data.armor.penalizeSpirit) {
+      data.computed.saves.spirit.penalty = 4;
+      data.computed.saves.spirit.save += 4;
+    }
+
+    data.computed.effort = {};
+    data.computed.effort.available =
+        data.effort.total - (
+            data.effort.active +
+            data.effort.scene +
+            data.effort.day
+        )
+    ;
+
+    data.computed.hp = {};
+    data.computed.hp.max = 8 + data.computed.attributes.con.mod + (
+        (data.level - 1) * (4 + Math.ceil(data.computed.attributes.con.mod / 2))
+    );
+  }
+
+  _prepareSave(src, dest, atts, name, att1, att2) {
+    dest[name] = {};
+    dest[name].base = 15 - Math.max(
+        atts[att1].mod,
+        atts[att2].mod
+    );
+    dest[name].penalty = 0;
+    dest[name].save = dest[name].base - src[name].bonus;
+  }
+  _prepareNpcData(actorData) {
+    const data = actorData.data;
+
+    // Make a new Object that holds computed data and keeps it separate from anything else
+    data.computed = {};
+
+    data.computed.effort = {};
+    data.computed.effort.available =
+        data.effort.total - (
+            data.effort.active +
+            data.effort.scene +
+            data.effort.day
+        )
+    ;
   }
 }
