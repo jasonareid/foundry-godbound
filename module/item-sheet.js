f68b4cb7ccff61fca71761df30e965f119d60124
Jason Reid <jason@jasonreid.com>
2020-10-12 18:19:45 -0400

    Bound Word Sheet

--- module/item-sheet.js
+++ module/item-sheet.js
@@ -34,7 +34,28 @@ export class GodboundItemSheet extends ItemSheet {
 	activateListeners(html) {
     super.activateListeners(html);
 
+    html.find('.item-chat').click(ev => {
+      const item = this.item;
+      ChatMessage.create({
+        content: `<div><h3>${item.name}</h3><p>${item.data.data.description}</p></div>`,
+      });
+    });
+
     // Everything below here is only needed if the sheet is editable
     if (!this.options.editable) return;
+    html.find('.item-maybe-delete').click(ev => {
+      if(this.maybeDeleteActive) {
+        html.find('.item-delete').hide();
+        this.maybeDeleteActive = false;
+      } else {
+        html.find('.item-delete').show();
+        this.maybeDeleteActive = true;
+      }
+    });
+    html.find('.item-delete').click(ev => {
+      if(this.item.actor) {
+        this.item.actor.deleteOwnedItem(this.item.id);
+      }
+    });
   }
 }
